05d488e36f8e8ee6d9bcfc0c169eac12a65727ac
arijeet <arijeet@sbox.ltd>
2023-04-21 21:38:19 +0530

    Passing the file path from Home screen to Image Editing screen.

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pictweak/screens/image_editing_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +10,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ImagePicker imagePicker = ImagePicker();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: IconButton(
+            onPressed: () async {
+              await imagePicker
+                  .pickImage(
+                source: ImageSource.gallery,
+              )
+                  .then((value) {
+                if (value != null) {
+                  Navigator.pushNamed(
+                    context,
+                    ImageEditingScreen.routeName,
+                    arguments: value.path,
+                  );
+                }
+              });
+            },
+            icon: const Icon(
+              Icons.upload_file,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/screens/image_editing_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImageEditingScreen extends StatefulWidget {
+  const ImageEditingScreen({super.key});
+
+  static const routeName = '/image-edit-screen';
+
+  @override
+  State<ImageEditingScreen> createState() => _ImageEditingScreenState();
+}
+
+class _ImageEditingScreenState extends State<ImageEditingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
